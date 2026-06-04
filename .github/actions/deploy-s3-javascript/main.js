6be1b3d8b232f821fcf1d649ecec6441686b6adf
Mayank <maykuu1088@gmail.com>
2026-06-05 00:50:09 +0530

    js action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,11 @@
+const core = require('@actions/core');
+
+const core = require('@actions/github');
+
+const core = require('@actions/exec');
+
+function run(){
+    core.notice('Hello son');
+}
+
+run();
